7549951a439dcbd28c50fbb6ee95a60f9508ef70
Marlene <ruthboko23@gmail.com>
2025-03-05 19:31:18 +0100

    Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'cors',
         ],
     ];
 
@@ -64,10 +65,11 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         //'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     protected $routeMiddleware = [
-       
+
         'api' => \App\Http\Middleware\AuthenticateApi::class,
     ];
 }
